4ae7a8bd50eee64620549364b8b0a3a4c68788e2
Dhananjay <79267037+dhannjayyy@users.noreply.github.com>
2023-09-06 13:43:19 +0530

    Trying firebase cloud functions

---
+++ functions/index.js
@@ -0,0 +1,38 @@
+const functions = require("firebase-functions");
+const express = require("express");
+const socket = require("socket.io");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.static("public"));
+
+const server = app.listen(port);
+
+const io = socket(server);
+
+io.on("connection", (socket) => {
+  console.log("New connection");
+
+  socket.on("beginPath", (coords) => {
+    io.sockets.emit("beginPath", coords);
+  });
+
+  socket.on("drawline", (data) => {
+    io.sockets.emit("drawline", data);
+  });
+
+  socket.on("undo", (data) => {
+    io.sockets.emit("undo", data);
+  });
+
+  socket.on("redo", (data) => {
+    io.sockets.emit("redo", data);
+  });
+
+  socket.on("resetCursor", () => {
+    io.sockets.emit("resetCursor");
+  });
+});
+
+exports.server = functions.https.onRequest(app);
